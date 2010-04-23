fa93551ca9a580fb13cfa1c6d768b7d0484a705b
alan.verga@gmail.com <alan.verga@gmail.com@3cc40782-2dda-c7f3-147a-e5b3b00d402f>
2010-04-23 10:21:54 +0000

    updated main to use test files and see AST output

--- src/plt/simetro/Backend.java
+++ src/plt/simetro/Backend.java
@@ -1,18 +1,30 @@
 package plt.simetro;
 
+import java.io.IOException;
+
+import org.antlr.runtime.ANTLRFileStream;
 import org.antlr.runtime.ANTLRStringStream;
 import org.antlr.runtime.CharStream;
 import org.antlr.runtime.CommonTokenStream;
 import org.antlr.runtime.RecognitionException;
 import org.antlr.runtime.TokenStream;
 import org.antlr.runtime.tree.CommonTree;
+import org.antlr.runtime.tree.CommonTreeNodeStream;
 
 public class Backend {
 
-		public static void main(String[] args) throws RecognitionException {
+		public static void main(String[] args) throws RecognitionException, IOException {
 			
-			String s  = "Population A{(A,10) (B, 23) (C, 19)}";
-			CharStream charStream = new ANTLRStringStream(s);
+			//String s  = "Population A{(A,10) (B, 23) (C, 19)}";
+			//CharStream charStream = new ANTLRStringStream(s);
+			
+			//String file = new String("./Testing/Basic_test.sim");
+			//String file = new String("./Testing/Conditional_test.sim");
+			String file = new String("./Testing/Loop_test.sim");
+			//String file = new String("./Testing/Object_test.sim");
+			//String file = new String("./Testing/smalltown.sim");
+			
+			CharStream charStream = new ANTLRFileStream(file);
 			simgramLexer lexer = new simgramLexer(charStream );
 
 			TokenStream tokenStream = new CommonTokenStream(lexer);
@@ -20,6 +32,7 @@ public class Backend {
 			
 			simgramParser.program_return r = parser.program();
 			CommonTree t = (CommonTree)r.getTree();
+			
 			
 			System.out.println(t.toStringTree());
 			
